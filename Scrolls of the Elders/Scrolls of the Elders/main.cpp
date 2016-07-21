df11d6101b161a75591973f487ac49eb858ee46a
WikiWiki47 <dpeebles5463@gmail.com>
2016-07-21 13:50:47 -0400

    Added a for loop

--- Scrolls of the Elders/Scrolls of the Elders/main.cpp
+++ Scrolls of the Elders/Scrolls of the Elders/main.cpp
@@ -6,6 +6,10 @@
 int main() {
 	int someNumber = 122;
 
+	for (int i = 0; i << 100; i++) {
+		std::cout << i << std::endl;	
+	}
+
 	// test pull before you push
 	std::cout << "Hello World" << std::endl;
 	system("pause");
